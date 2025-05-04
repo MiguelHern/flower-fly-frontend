a7f7359a95f78ff485e1cf7e2a3e99e674218fa5
samuelopez <lopez180219@gmail.com>
2025-05-04 11:39:10 -0600

    Modificación login

--- src/features/auth/components/LoginForm.vue
+++ src/features/auth/components/LoginForm.vue
@@ -1,54 +1,60 @@
 <script setup>
 import router from '@/router/index.js'
-import ButtonBase from '@/components/ui/ButtonBase.vue'
+import { Flower2 } from 'lucide-vue-next'
 </script>
 
 <template>
-  <h2 class="text-2xl font-semibold mb-4">Inicia sesión</h2>
-  <form action="#" method="POST">
-    <div class="mb-4">
-      <label for="username" class="form-label">Correo</label>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        class="form-input"
-        autocomplete="off"
-        placeholder="Correo"
-      />
-    </div>
-    <div class="mb-4">
-      <label for="email" class="form-label">
-        Correo electrónico
-      </label>
-      <input
-        id="email"
-        type="email"
-        placeholder="ejemplo@correo.com"
-        class="form-input"
-      />
-    </div>
-    <!--    <div class="mb-4 flex items-center">-->
-    <!--      <input type="checkbox" id="remember" name="remember" class="text-blue-500">-->
-    <!--      <label for="remember" class="text-gray-600 ml-2">Remember Me</label>-->
-    <!--    </div>-->
+  <div class="w-full flex items-center justify-center">
+    <div class="w-[70%]">
+      <div class="mb-7">
+        <div class="flex items-center justify-center mb-2">
+          <Flower2 class="h-10 w-10 text-pink-500" />
+        </div>
+        <h1 class="text-3xl text-center font-bold text-pink-700">Florería Flower Fly</h1>
+        <h3 class="text-center text-gray-500">Ingresa a tu cuenta para realizar pedidos</h3>
+      </div>
+      <form @submit.prevent="handleSubmit" class="space-y-4">
+        <div class="space-y-2">
+          <label for="email" class="block text-sm font-medium">Correo electrónico</label>
+          <input
+            id="email"
+            type="email"
+            v-model="email"
+            placeholder="tu@email.com"
+            required
+            class="text-sm text-gray-400 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-600"
+          />
+        </div>
+        <div class="space-y-2">
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium">Contraseña</label>
+            <a href="#" class="text-sm text-pink-600 hover:text-pink-800">
+              ¿Olvidaste tu contraseña?
+            </a>
+          </div>
+          <input
+            id="password"
+            type="password"
+            v-model="password"
+            required
+            class="text-sm text-gray-400 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-600"
+          />
+        </div>
+        <button
+          type="submit"
+          class="w-full cursor-pointer py-2 px-4 bg-pink-600 hover:bg-pink-700 text-white font-medium rounded-md"
+        >
+          Iniciar sesión
+        </button>
+      </form>
 
-    <!--    <div class="mb-6 text-blue-500">-->
-    <!--      <a href="#" class="hover:underline">Forgot Password?</a>-->
-    <!--    </div>-->
-    <ButtonBase
-      type="submit"
-      class="bg-black hover:bg-black/80 text-white font-semibold rounded-md py-2 px-4 w-full"
-    >
-      Login
-    </ButtonBase>
-  </form>
-  <!-- Sign up  Link -->
-  <div class="mt-6 text-center">
-    <span class="text-gray-500">¿No tienes cuenta? </span>
-    <button @click="router.push({ name: 'register' })" class="text-gray-900 dark:text-white hover:underline">
-      Regístrate
-    </button>
+      <div class="mt-6 text-center">
+        <span class="text-sm text-gray-500">¿No tienes cuenta? </span>
+        <button @click="router.push({ name: 'register' })" class="text-sm cursor-pointer text-gray-900 dark:text-white hover:underline">
+          Regístrate
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,17 +1,20 @@
 <script setup>
-
+import { Flower2 } from 'lucide-vue-next';
 </script>
 
 <template>
   <!-- component -->
-  <div class="bg-gray-100 flex justify-center items-center h-screen">
+  <div class="bg-white flex justify-center items-center h-screen">
     <!-- Left: Image -->
-    <div class="w-1/2 h-screen hidden lg:block">
-      <img src="https://res.cloudinary.com/djfokdod6/image/upload/f_webp,q_auto:good,w_500/v1746248087/31f88fa2944166e13d1d42e57cab31e9e5a36ab8_ojv02a.png" alt="Floreria" class="object-cover w-full h-full">
+    <div class="w-full h-full md:w-1/2 relative flex items-center justify-center">
+      <img src="/src/assets/img/flowers.png" alt="Arreglo floral hermoso" class="w-2xl object-cover" />
+      <div class="absolute inset-0 bg-gradient-to-t from-pink-500/60 to-transparent flex flex-col items-center justify-end pb-8 px-4">
+        <h2 class="text-white text-5xl font-bold text-center drop-shadow-lg mb-2">Flower Fly</h2>
+        <p class="text-white text-xl text-center drop-shadow-md mb-6">"Flores que hablan por ti"</p>
+      </div>
     </div>
-    <!-- Right: Login Form -->
     <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2 h-full ">
-      <h1 class="text-center text-5xl">Flower Fly</h1>
+
       <div class="flex flex-col justify-center h-full">
         <router-view/>
       </div>
